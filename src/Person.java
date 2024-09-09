01bb7463b7d0940ac3a2d4eb66aab793c3d61b29
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2024-09-09 19:41:43 -0400

    Adding Getters and Setters

--- src/Person.java
+++ src/Person.java
@@ -4,5 +4,53 @@ public class Person {
     private String ID;
     private String title;
     private int YOB;
-    private String hi;
+
+    /*Constructor*/
+    public Person(String firstName, String lastName, String ID, String title, int YOB) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ID = ID;
+        this.title = title;
+        this.YOB = YOB;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
 }
